e891ff04ff3a58aadcd3cc568f80262bb9448aba
andres2392 <32281805+andres2392@users.noreply.github.com>
2022-10-07 15:11:21 -0400

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by PDWS on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
